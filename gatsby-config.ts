bcd3749b85495034c54fe995778a571aabee6e96
Gu-Parlandim <56051040+Gu-Parlandim@users.noreply.github.com>
2024-05-01 15:16:06 -0300

    chore: install gatsby

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,18 +3,37 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `.`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }]
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+    {
+      resolve: "gastby-plugin-imports",
+      options: {
+        alias: {
+          "@components": "src/components",
+          "@images": "src/images",
+          "@pages": "src/pages",
+          "@types": "src/types",
+          "@utils": "src/utils",
+          "@src": "src",
+          posts: "content/posts",
+        },
+        extensions: ["js", "jsx", "ts", "tsx"],
+      },
+    },
+  ],
 };
 
 export default config;
