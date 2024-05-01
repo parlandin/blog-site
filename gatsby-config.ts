8282d9451c20a9be1fe60f4781f4385744730456
Gu-Parlandim <56051040+Gu-Parlandim@users.noreply.github.com>
2024-05-01 15:02:36 -0300

    Initial commit from gatsby

---
+++ gatsby-config.ts
@@ -0,0 +1,20 @@
+import type { GatsbyConfig } from "gatsby";
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `.`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+  // If you use VSCode you can also use the GraphQL plugin
+  // Learn more at: https://gatsby.dev/graphql-typegen
+  graphqlTypegen: true,
+  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-sitemap", {
+    resolve: 'gatsby-plugin-manifest',
+    options: {
+      "icon": "src/images/icon.png"
+    }
+  }]
+};
+
+export default config;
