3a6d70bbefe75d9c9ece9437ef1e810c32c6d4d1
Gu-Parlandim <56051040+Gu-Parlandim@users.noreply.github.com>
2024-05-03 01:47:32 -0300

    feat: create coming soon component

---
+++ src/components/ComingSoon/index.tsx
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+import React from "react";
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const Title = styled.h1`
+  font-size: 3rem;
+  font-weight: 700;
+  color: ${({ theme }) => theme.colors.primary};
+`;
+
+const ComingSoon: React.FC = () => {
+  return (
+    <Container>
+      <Title>
+        Em breve{" "}
+        <span role="img" aria-label="emoji">
+          🚧
+        </span>
+      </Title>
+    </Container>
+  );
+};
+
+export default ComingSoon;
