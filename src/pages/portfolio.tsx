4a3a49adcbc1d0decf0508f2eb43c0c0afb4ffcf
Gu-Parlandim <56051040+Gu-Parlandim@users.noreply.github.com>
2024-06-22 01:27:48 -0300

    feat: add portolio page

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -1,10 +1,71 @@
 import * as React from "react";
-import type { PageProps } from "gatsby";
-
-import ComingSoon from "@components/ComingSoon";
+import type { HeadFC, PageProps } from "gatsby";
+import { VerticalTimelineElement } from "react-vertical-timeline-component";
+import * as S from "@styles/portfolioStyle";
+import Title from "@components/Title";
+import { StaticImage } from "gatsby-plugin-image";
+import useProjectsData from "@hooks/useProjectsData";
+import "react-vertical-timeline-component/style.min.css";
+import SEO from "@components/SEO";
 
 const PortfolioPage: React.FC<PageProps> = () => {
-  return <ComingSoon />;
+  const projects = useProjectsData();
+  return (
+    <S.Container>
+      <Title>Meus projetos</Title>
+      <S.AboutText>
+        Aqui você encontra todos os meus projetos em ordem cronologicamente
+        !!incorreta.
+      </S.AboutText>
+
+      <S.TimelineContainer animate={true} layout="2-columns">
+        {projects.map((project) => (
+          <VerticalTimelineElement
+            key={project.id}
+            className="vertical-timeline-element"
+            contentArrowStyle={{ borderRight: "10px solid  " }}
+            date={project.date}
+            iconStyle={{
+              background: "transparent",
+              width: "30px",
+              height: "30px",
+              alignItems: "center",
+              alignSelf: "center",
+            }}
+            icon={project.icon}
+          >
+            <S.ProjectContainer>
+              <S.ProjectImage>
+                <StaticImage
+                  src="../images/projects/react-js.png"
+                  alt={project.title}
+                  placeholder="blurred"
+                />
+              </S.ProjectImage>
+
+              <S.ProjectContent>
+                <S.ProjectTitle>{project.title}</S.ProjectTitle>
+
+                <S.ProjectDescription>
+                  {project.description}
+                </S.ProjectDescription>
+
+                <S.TagsContainer>
+                  {project.tags.map((tag) => (
+                    <S.Tag key={tag}>{tag}</S.Tag>
+                  ))}
+                </S.TagsContainer>
+              </S.ProjectContent>
+            </S.ProjectContainer>
+          </VerticalTimelineElement>
+        ))}
+      </S.TimelineContainer>
+    </S.Container>
+  );
+};
+
+export const Head: HeadFC = () => {
+  return <SEO title="Portfolio" />;
 };
 
 export default PortfolioPage;
---
+++ src/styles/portfolioStyle.ts
@@ -0,0 +1,127 @@
+import styled from "styled-components";
+import { VerticalTimeline } from "react-vertical-timeline-component";
+
+export const Container = styled.section`
+  height: 100%;
+  width: 100%;
+`;
+export const AboutText = styled.p`
+  font-size: 1rem;
+  margin: 15px 0 25px;
+  color: ${(props) => props.theme.colors.text};
+`;
+
+export const TimelineContainer = styled(VerticalTimeline)`
+  &::before {
+    background-color: ${(props) => props.theme.colors.primary} !important;
+    width: 2px !important;
+  }
+
+  & .vertical-timeline-element {
+    & .vertical-timeline-element-content-arrow {
+      top: 10px !important;
+      border-right-color: ${(props) => props.theme.colors.primary} !important;
+      right: 100.3% !important;
+    }
+
+    & .vertical-timeline-element-content {
+      padding: 0px !important;
+      border: 3px solid ${(props) => props.theme.colors.primary} !important;
+      background-color: transparent !important;
+      border-bottom: none !important;
+      box-shadow: 0 3px 0 ${(props) => props.theme.colors.text};
+      max-width: 500px !important;
+
+      & .vertical-timeline-element-date {
+        color: ${(props) => props.theme.colors.text} !important;
+
+        @media only screen and (min-width: 1170px) {
+          top: -9px;
+        }
+      }
+    }
+
+    & .vertical-timeline-element-icon {
+      box-shadow: 0 0 0 2px ${(props) => props.theme.colors.text},
+        inset 0 2px 0 rgba(0, 0, 0, 0.08), 0 3px 0 4px rgba(0, 0, 0, 0.05);
+      background: ${(props) => props.theme.colors.secondary} !important;
+      left: 3px !important;
+      color: #fff !important;
+
+      &.vertical-timeline--two-columns {
+        left: 52.4% !important;
+      }
+
+      @media only screen and (min-width: 1170px) {
+        left: 52.33% !important;
+        margin-left: -30px;
+      }
+    }
+  }
+`;
+
+export const ProjectContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  cursor: pointer;
+`;
+
+export const ProjectImage = styled.div`
+  width: 100%;
+  height: 200px;
+  border-radius: 0.25em;
+  border-bottom-left-radius: 0;
+  border-bottom-right-radius: 0;
+  overflow: hidden;
+
+  & img {
+    border-radius: 0.25em;
+    max-width: 100%;
+    max-height: 100%;
+    object-fit: cover;
+    border-bottom-left-radius: 0;
+    border-bottom-right-radius: 0;
+  }
+`;
+
+export const ProjectContent = styled.div`
+  font-size: 1rem;
+  padding: 10px;
+`;
+
+export const ProjectTitle = styled.h3`
+  font-size: 1.1rem;
+  color: ${(props) => props.theme.colors.primary};
+  font-weight: 600;
+`;
+
+export const ProjectDescription = styled.p`
+  font-size: 0.9rem !important;
+  color: ${(props) => props.theme.colors.text};
+  margin: 10px 0;
+  -webkit-line-clamp: 4 !important;
+  -webkit-box-orient: vertical !important;
+  overflow: hidden !important;
+  text-overflow: ellipsis !important;
+
+  @media only screen and (min-width: 1170px) {
+    max-height: 100px !important;
+  }
+`;
+
+export const TagsContainer = styled.section`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 5px 8px;
+  margin-top: 10px;
+  //margin-top: 15px;
+`;
+
+export const Tag = styled.span`
+  font-size: 0.8rem;
+  color: ${(props) => props.theme.colors.text};
+  background-color: ${(props) => props.theme.colors.tertiary};
+  border-radius: 4px;
+  padding: 4px 8px;
+`;
