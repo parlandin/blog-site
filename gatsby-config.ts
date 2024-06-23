7652ce8d1167ebda7d2973c28c631a69d4c2c4bd
Gu-Parlandim <56051040+Gu-Parlandim@users.noreply.github.com>
2024-06-23 17:08:17 -0300

    feat: add mdx to get dynamic project data

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -31,6 +31,13 @@ const config: GatsbyConfig = {
     {
       resolve: "gatsby-source-filesystem",
       options: {
+        name: `projects`,
+        path: `${__dirname}/content/projects`,
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
         name: `images`,
         path: `${__dirname}/src/images`,
       },
