7e4d28eefca96f86b654fc939a06f89ddbe8b710
parlandin <56051040+parlandin@users.noreply.github.com>
2024-10-17 16:35:36 -0300

    create config file and theme events file

---
+++ src/configs/eventThemes.ts
@@ -0,0 +1,49 @@
+const eventThemes = {
+  default: {
+    name: "Default",
+    header: {
+      icon: "icon-default-png",
+      alt: "confetti ball",
+    },
+    hero: {
+      image: "",
+      alt: "",
+      quotes: "defaultQuotes",
+    },
+    wordOfTheDay: {
+      image: "",
+      alt: "",
+    },
+    notFound: {
+      image: "",
+      alt: "",
+      text: "defaultNotFoundText",
+    },
+    colorTheme: "default",
+  },
+
+  halloween: {
+    name: "alloween",
+    header: {
+      icon: "icon-halloween-png",
+      alt: "pumpkin",
+    },
+    hero: {
+      image: "",
+      alt: "",
+      quotes: "halloweenQuotes",
+    },
+    wordOfTheDay: {
+      image: "",
+      alt: "",
+    },
+    notFound: {
+      image: "",
+      alt: "",
+      text: "halloweenNotFoundText",
+    },
+    colorTheme: "halloween",
+  },
+};
+
+export default eventThemes;
---
+++ src/configs/general.ts
@@ -0,0 +1,13 @@
+import eventThemes from "./eventThemes";
+
+const config = {
+  defaultLanguage: "pt-BR",
+
+  //
+  currentEventTheme: eventThemes.halloween,
+  isEventThemeActive: true,
+
+  backendUrl: "",
+};
+
+export default config;
